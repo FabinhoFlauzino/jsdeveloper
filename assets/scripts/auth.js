177fb4af3830635cbac752480083458e382c272c
Fabinho Flauzino <fabinhoflauzino.dev@gmail.com>
2021-01-11 21:31:00 -0300

    first commit

---
+++ assets/scripts/auth.js
@@ -0,0 +1,67 @@
+const authPage = document.querySelector("main#auth");
+
+if (authPage) {
+    const hideAuthForms = () => {
+        document
+            .querySelectorAll("#auth form")
+            .forEach((el) => el.classList.add("hide"));
+    };
+
+    const showAuthForm = (id) => {
+        document.getElementById(id).classList.remove("hide");
+    };
+
+    const authHash = () => {
+        hideAuthForms();
+
+        if (sessionStorage.getItem("email")) {
+            document
+                .querySelectorAll("[name=email]")
+                .forEach((el) => (el.value = sessionStorage.getItem("email")));
+        }
+
+        //analise o hash na url da window. window.location.hash
+
+        switch (window.location.hash) {
+            case "#register":
+                showAuthForm("register");
+                break;
+
+            case "#login":
+                showAuthForm("login");
+                break;
+
+            case "#forget":
+                showAuthForm("forget");
+                break;
+
+            case "#reset":
+                showAuthForm("reset");
+
+            default:
+                showAuthForm("auth-email");
+        }
+    };
+
+    window.addEventListener("load", (e) => {
+        authHash();
+    });
+
+    window.addEventListener("hashcange", (e) => {
+        authHash();
+    });
+
+    const formAuthEmail = document.querySelector("#auth-email");
+
+    formAuthEmail.addEventListener("submit", (e) => {
+        e.preventDefault();
+        //e.stopPropagation()
+
+        const btnSubmit = e.target.querySelector("[type=submit]");
+        btnSubmit.desabled = true;
+
+        sessionStorage.setItem("email", formAuthEmail.email.value);
+        location.hash = "#login";
+        btnSubmit.desabled = false;
+    });
+}
